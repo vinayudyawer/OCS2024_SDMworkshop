824672c4c211473fe33197de99bb9157dd6405e1
Vinay Udyawer <vinay.udyawer@gmail.com>
2024-03-21 11:03:07 +1000

    update session 4

--- R/2024-03-21_Code for session 4.R
+++ R/2024-03-21_Code for session 4.R
@@ -1,46 +1,65 @@
 ## Session 4 script
 
 library(tidyverse)
-library(sf)
 library(terra)
 library(ggspatial)
 
+## add land for remove points on land (internal only not for the course)
+## Land data
+# sf_use_s2(FALSE)
+# 
+# land <- 
+#   rnaturalearth::ne_countries(scale = "medium", returnclass = "sf") %>% 
+#   st_wrap_dateline(options = c("WRAPDATELINE=YES")) %>%
+#   st_set_crs(4326) %>% 
+#   summarise(across(geometry, ~ sf::st_union(.)), .groups = "keep") %>% 
+#   st_make_valid()
+# 
+# sf_use_s2(TRUE)
+
+
 ## Input occurrence data
 
-tracks <- read_csv('https://raw.githubusercontent.com/vinayudyawer/OCS2024_SDMworkshop/main/data/session_2/mp_fit_pred.csv')
-null_tracks <- read_csv('https://raw.githubusercontent.com/vinayudyawer/OCS2024_SDMworkshop/main/data/session_2/null_data.csv')
+tracks <- read_csv('https://raw.githubusercontent.com/vinayudyawer/OCS2024_SDMworkshop/main/data/session_4/track_data.csv')
+
+null_tracks <- read_csv('https://raw.githubusercontent.com/vinayudyawer/OCS2024_SDMworkshop/main/data/session_4/null_data.csv')
+
+background <- read_csv('https://raw.githubusercontent.com/vinayudyawer/OCS2024_SDMworkshop/main/data/session_4/bg_data.csv')
 
 model_extent <- vect("https://raw.githubusercontent.com/vinayudyawer/OCS2024_SDMworkshop/main/data/session_3/mod_ext.GeoJSON")
-
-null_sf <-
-  null_tracks %>% 
-  st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = F) %>% 
-  vect()
-
-bg <-
-  model_extent %>% st_as_sf() %>% 
-  st_sample(1500) %>% 
-  vect() %>% 
-  difference(env_stack[[1]])
-
-plot(bg, col = "grey")
-plot(model_extent, add = T)
-plot(null_sf ,add = T)
 
 
 # quick plot to show the presence and pseudo absences
 
 ggplot() +
-  annotation_map_tile()
+  annotation_map_tile('cartolight', zoom = 4) +
+  layer_spatial(model_extent, fill = NA) +
+  geom_spatial_point(data = background, aes(x = lon, y = lat), col = "grey", crs = 4326) +
+  geom_spatial_point(data = null_tracks, aes(x = lon, y = lat), col = "pink", crs = 4326) +
+  geom_spatial_point(data = tracks, aes(x = lon, y = lat), col = "firebrick", crs = 4326) +
+  theme_void()
 
 
 
 ## Input environmental data
-
 env_stack <- rast("https://raw.githubusercontent.com/vinayudyawer/OCS2024_SDMworkshop/main/data/session_3/env_layers.tif")
 
+# quick plot
 
-## 
+plot(env_stack)
+
+
+
+## extract environmental data for each occurrence, pseudo-absence and background point
+
+tracks_vec <- vect(tracks, geom=c("lon", "lat"), crs = "EPSG:4326")
+null_vec <- vect(null_tracks, geom=c("lon", "lat"), crs = "EPSG:4326")
+bg_vec <- vect(tracks, geom=c("lon", "lat"), crs = "EPSG:4326")
+
+
+## Now lets build a model
+
+
 
 
 
